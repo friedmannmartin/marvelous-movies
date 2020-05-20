a7ae65755047f127f6191e2c3edad7ee621b30ce
Martin Friedmann <friedmann_martin@outlook.com>
2020-05-21 01:25:47 +0200

    Přidání funkcionality zapomenutého hesla

---
+++ password-renew.php
@@ -0,0 +1,121 @@
+<?php
+    /* Authorization and Authentication */
+    require_once './include/auth.php';
+    unauthorizedUsersOnly();
+
+    /* If there is posted form try renew password */
+    $invalidCode=false;
+    $invalidPassword=false;
+    if (!empty($_REQUEST) && !empty($_REQUEST['code'])){
+
+        /* Searching for record of forgotten password request */
+        $query=$db->prepare('SELECT * FROM forgotten_passwords WHERE forgotten_password_id=:id AND code=:code AND user_id=:user LIMIT 1;');
+        $query->execute([
+            ':user'=>$_REQUEST['user'],
+            ':code'=>$_REQUEST['code'],
+            ':id'=>$_REQUEST['request'],
+        ]);
+        if ($existingRequest=$query->fetch(PDO::FETCH_ASSOC)){
+            /* Checking whether request is stil valid */
+            if (strtotime($existingRequest['created'])<(time()-24*3600)){
+                /* Request has expired */
+                $invalidCode=true;
+            }
+        } else{
+            /* Request not found */
+            $invalidCode=true;
+        }
+
+        if (!empty($_POST) && !$invalidCode){
+
+            /* New password validation */
+            if (empty($_POST['password']) || (strlen($_POST['password'])<5)){
+                $invalidPassword='Password needs to be at least 5 characters long.';
+            }
+            if ($_POST['password']!=$_POST['password2']){
+                $invalidPassword='Passwords do not match.';
+            }
+
+            /* Updating new password in DB */
+            if (!$invalidPassword){
+                $saveQuery=$db->prepare('UPDATE users SET password=:password WHERE user_id=:user LIMIT 1;');
+                $saveQuery->execute([
+                    ':user'=>$existingRequest['user_id'],
+                    ':password'=>password_hash($_POST['password'], PASSWORD_DEFAULT)
+                ]);
+
+                /* Delete requests for password reset */
+                $forgottenDeleteQuery=$db->prepare('DELETE FROM forgotten_passwords WHERE user_id=:user;');
+                $forgottenDeleteQuery->execute([':user'=>$existingRequest['user_id']]);
+
+                /* Getting currend user */
+                $userQuery=$db->prepare('SELECT * FROM users WHERE user_id=:user LIMIT 1;');
+                $userQuery->execute([
+                    ':user'=>$existingRequest['user_id']
+                ]);
+                $user=$userQuery->fetch(PDO::FETCH_ASSOC);
+
+                /* Signing in current user */
+                $_SESSION['user_id']=$user['user_id'];
+                $_SESSION['user_name']=$user['name'];
+
+                header('Location: ./');
+                exit();
+            }
+        }
+    }
+
+    $pageTitle = 'Password renew';
+
+    include './include/header.php';
+?>
+
+<main class="d-flex align-items-center">
+    <div class="container-sm py-5">
+        <h2 class="pb-3">Password renew</h2>
+        <?php if($invalidCode): ?>
+            <p class="alert alert-danger">Password renew request expired!</p>
+            <a href="./" class="btn btn-primary">Return to Homepage</a>
+        <?php else: ?>
+        <form method="post">
+
+            <!-- Password -->
+            <div class="form-group">
+                <label for="password">New password:</label>
+                <input type="password"
+                       name="password"
+                       id="password"
+                       placeholder="Enter your new password"
+                       autocomplete="new-password"
+                       required
+                       class="form-control <?php echo ($invalidPassword ? 'is-invalid':'') ?>"/>
+                <?php if ($invalidPassword):?>
+                  <div class="invalid-feedback"><?=$invalidPassword?></div>
+                <?php endif ?>
+            </div>
+
+            <!-- Password confirmation -->
+            <div class="form-group">
+                <label for="password2">Password confirmation:</label>
+                <input type="password"
+                        name="password2"
+                        id="password2"
+                        placeholder="Enter your new password once more"
+                        required
+                        class="form-control <?php echo ($invalidPassword?'is-invalid':'') ?>" />
+            </div>
+
+            <input type="hidden" name="code"    value="<?= htmlspecialchars($_REQUEST['code']) ?>" />
+            <input type="hidden" name="user"    value="<?= htmlspecialchars($_REQUEST['user']) ?>" />
+            <input type="hidden" name="request" value="<?= htmlspecialchars($_REQUEST['request']) ?>" />
+
+            <button type="submit" class="btn btn-primary">Change password</button>
+            <hr>
+            <a href="./sign-in"   class="btn btn-light">Sign In</a>
+            <a href="./" class="btn btn-light">Cancel</a>
+        </form>
+        <?php endif ?>
+    </div>
+</main>
+
+<?php include './include/footer.php'; ?>
---
+++ password-reset.php
@@ -0,0 +1,102 @@
+<?php
+    /* Authorization and Authentication */
+    require_once './include/auth.php';
+    unauthorizedUsersOnly();
+
+    /* Prepare PHPMailer */
+    use PHPMailer\PHPMailer\PHPMailer;
+
+    /* If there is posted form try send email to user */
+    $errors=false;
+    if (!empty($_POST) && !empty($_POST['email'])){
+        /* Searching user in DB by posted email */
+        $userQuery=$db->prepare('SELECT * FROM users WHERE email=:email LIMIT 1;');
+        $userQuery->execute([
+            ':email'=>trim($_POST['email'])
+        ]);
+        if ($user=$userQuery->fetch(PDO::FETCH_ASSOC)){
+            /* If email address found generate random code */
+            $code='xx'.rand(100000,993952);
+
+            /* Create record in DB for password reset */
+            $saveQuery=$db->prepare('INSERT INTO forgotten_passwords (user_id, code) VALUES (:user, :code)');
+            $saveQuery->execute([
+                ':user'=>$user['user_id'],
+                ':code'=>$code
+            ]);
+
+            /* Getting record of forgotten password */
+            $requestQuery=$db->prepare('SELECT * FROM forgotten_passwords WHERE user_id=:user_id AND code=:code ORDER BY forgotten_password_id DESC LIMIT 1;');
+            $requestQuery->execute([
+                ':user_id'=>$user['user_id'],
+                ':code'=>$code
+            ]);
+            $request=$requestQuery->fetch(PDO::FETCH_ASSOC);
+
+            /* Creating unique link to renew password  */
+            $link='https://eso.vse.cz/~frim00/marvelous-movies/password-renew';
+            $link.='?user='.$request['user_id'].'&code='.$request['code'].'&request='.$request['forgotten_password_id'];
+
+            /* Creating email */
+            $mailer=new PHPMailer(false);
+            $mailer->isSendmail();
+            $mailer->addAddress($user['email'],$user['name']);
+            $mailer->setFrom('frim00@vse.cz');
+            $mailer->CharSet='utf-8';
+            $mailer->Subject='Forgotten password reset';
+            $mailer->isHTML(true);
+            $mailer->Body ='<html>
+                                <head><meta charset="utf-8" /></head>
+                                <body>
+                                    To reset your Marvelous Movies password, click the following link:
+                                    <a href="'.htmlspecialchars($link).'">'.htmlspecialchars($link).'</a>
+                                </body>
+                            </html>';
+            $mailer->AltBody='To reset your Marvelous Movies password, click the following link: '.$link;
+            $mailer->send();
+
+            header('Location: password-reseted');
+        } else{
+            /* No account with posted email address found */
+            $errors=true;
+        }
+    }
+
+    $pageTitle = 'Password reset';
+
+    include './include/header.php';
+?>
+
+<main class="d-flex align-items-center">
+  <div class="container-sm py-5">
+    <h2 class="pb-3">Forgotten password reset</h2>
+        <?php if(@$_GET['mailed']=='ok'): ?>
+            <p>Check your inbox for e-mail with link, which will allow you to change your password.</p>
+            <a href="./" class="btn btn-primary">Go back to homepage</a>
+        <?php else: ?>
+        <form method="post">
+
+            <div class="form-group">
+                <label for="email">E-mail:</label>
+                <input type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Enter your e-mail"
+                    required
+                    class="form-control <?php echo ($errors?'is-invalid':''); ?>"
+                    value="<?php echo htmlspecialchars(@$_POST['email'])?>"/>
+                <?php if($errors): ?>
+                    <div class="invalid-feedback">You entered invalid email address.</div>
+                <?php endif ?>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Send me email for password reset</button>
+            <hr>
+            <a href="./sign-in"   class="btn btn-light">Sign In</a>
+            <a href="./"          class="btn btn-light">Cancel</a>
+        </form>
+        <?php endif ?>
+  </div>
+</main>
+
+<?php include './include/footer.php'; ?>
