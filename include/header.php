597155a29db673a7fccf637b40da7e137d994e1c
Martin Friedmann <friedmann_martin@outlook.com>
2020-05-20 23:28:40 +0200

    BugFix

--- include/header.php
+++ include/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <base href="https://eso.vse.cz/~frim00/marvelous-movies-2/">
+    <base href="https://eso.vse.cz/~frim00/marvelous-movies/">
 
     <!-- CSS -->
     <link rel="stylesheet" href="./css/style.css">
