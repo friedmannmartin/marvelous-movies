1da3f95593c4f21fffefeaf0210a90bb819ec215
Martin Friedmann <friedmann_martin@outlook.com>
2020-05-22 00:37:45 +0200

    Oprava nevalidní html entity

--- include/footer.php
+++ include/footer.php
@@ -1,1 +1,1 @@
-<footer class="bg-dark text-light text-center py-3">Created by <a class="text-info" href="mailto:frim00@vse.cz">Martin Friedmann</a> &copy 2020</footer></body></html>
+<footer class="bg-dark text-light text-center py-3">Created by <a class="text-info" href="mailto:frim00@vse.cz">Martin Friedmann</a> &copy; 2020</footer></body></html>
