54cfaea13c7f90417a1c93c84988417c57313666
Martin Friedmann <friedmann_martin@outlook.com>
2020-05-23 23:03:01 +0200

    Přidání funkcionality vlastních tooltipů

--- include/footer.php
+++ include/footer.php
@@ -1,1 +1,7 @@
-<footer class="bg-dark text-light text-center py-3">Created by <a class="text-info" href="mailto:frim00@vse.cz">Martin Friedmann</a> &copy; 2020</footer></body></html>
+        <footer class="bg-dark text-light text-center py-3">
+            Created by <a class="text-info" href="mailto:frim00@vse.cz">Martin Friedmann</a> &copy; 2020
+        </footer>
+        <!-- JS which enables custom Popper/Bootstrap tooltips -->
+        <script src="./script.js"></script>
+    </body>
+</html>
