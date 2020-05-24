f02eb1d0b905c46a07d53cf68e25a05e43a08c6b
Martin Friedmann <friedmann_martin@outlook.com>
2020-05-24 16:31:39 +0200

    ASCII Easter eggs

--- sign-out.php
+++ sign-out.php
@@ -1,4 +1,15 @@
 <?php
+
+  /*
+   * ,adPPYYba, 8b       d8  ,adPPYba, 8b,dPPYba,   ,adPPYb,d8  ,adPPYba, 8b,dPPYba, ,adPPYba,
+   * ""     `Y8 `8b     d8' a8P_____88 88P'   `"8a a8"    `Y88 a8P_____88 88P'   "Y8 I8[    ""
+   * ,adPPPPP88  `8b   d8'  8PP""""""" 88       88 8b       88 8PP""""""" 88          `"Y8ba,
+   * 88,    ,88   `8b,d8'   "8b,   ,aa 88       88 "8a,   ,d88 "8b,   ,aa 88         aa    ]8I
+   * `"8bbdP"Y8     "8"      `"Ybbd8"' 88       88  `"YbbdP"Y8  `"Ybbd8"' 88         `"YbbdP"'
+   *                                                aa,    ,88
+   *                                                 "Y8bbdP"
+   */
+
   session_start();
   session_destroy();
 
